e06690ab2352d4360b7f2e990a706b88838b93b0
bderembl <bruno.deremble@ens.fr>
2019-08-26 17:11:22 +0200

    gyre noxios

--- GYRE_noxios/scripts/mygendata.py
+++ GYRE_noxios/scripts/mygendata.py
@@ -38,7 +38,7 @@ xc,yc = np.meshgrid(xx1,yy1)
 dx1 = dx*np.ones((si_x))
 dy1 = dy*np.ones((si_y))
 
-dz1 = np.array([10,20,30,40,50,60,70,80,90,100,150,200,250,300,350,400,500,500,500])
+dz1 = np.array([10,20,30,40,50,60,70,80,90,100,150,200,250,300,350,400,500,500,500,500,500])
 np.savetxt(dir0 + 'dz.dat',dz1)
 
 ####### forcing and initial conditions
